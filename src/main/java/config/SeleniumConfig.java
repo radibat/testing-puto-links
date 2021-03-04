ff86c98a2f3ed0497fa892055edea6322725008c
radibat <radmiribatullin@gmail.com>
2021-03-04 12:24:45 +0500

    update SeleniumConfig.java

--- src/main/java/config/SeleniumConfig.java
+++ src/main/java/config/SeleniumConfig.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
@@ -16,8 +17,11 @@ public class SeleniumConfig {
     static String driverName;
 
     public SeleniumConfig() {
-        Capabilities capabilities = DesiredCapabilities.chrome();
-        driver = new ChromeDriver(capabilities);
+        //Capabilities capabilities = DesiredCapabilities.chrome();
+        ChromeOptions options = new ChromeOptions();
+        System.out.println("Создаем драйвер");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
